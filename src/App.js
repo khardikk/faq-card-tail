efe4afba7f5e971e3a8fcd472b994a0bfe283233
Hardikk <kambojpushkal@gmail.com>
2023-10-17 19:52:58 +0530

    done with normal css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
 
+import iconArrowDown from './images/icon-arrow-down.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const toggleAccordion = (index) => {
+    if (activeItem === index) {
+      setActiveItem(null); // Collapse the item if it's already open
+    } else {
+      setActiveItem(index); // Expand the clicked item
+    }
+  };
+
+  const accordionData = [
+    {
+      question: "How many team members can I invite?",
+      answer: "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+    },
+    {
+      question: "What is the maximum file upload size?",
+      answer: "No more than 2GB. All files in your account must fit your allotted storage space.",
+    },
+    {
+      question: "How do I reset my password?",
+      answer: "Click 'Forgot password' from the login page or 'Change password' from your profile page. A reset link will be emailed to you.",
+    },
+    {
+      question: "Can I cancel my subscription?",
+      answer: "Yes! Send us a message, and we'll process your request, no questions asked.",
+    },
+    {
+      question: "Do you provide additional support?",
+      answer: "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+    },
+  ];
+
   return (
-    <>
-    <p className='bg-slate-900 text-white' >
-    
-    How many team members can I invite?
-    You can invite up to 2 additional users on the Free plan. There is no limit on 
-    team members for the Premium plan.
-  
-    What is the maximum file upload size?
-    No more than 2GB. All files in your account must fit your allotted storage space.
-  
-    How do I reset my password?
-    Click “Forgot password” from the login page or “Change password” from your profile page.
-    A reset link will be emailed to you.
-  
-    Can I cancel my subscription?
-    Yes! Send us a message and we'll process your request no questions asked.
-  
-    Do you provide additional support?
-    Chat and email support is available 24/7. Phone lines are open during normal business hours.
-    </p>
-    {/* <div class="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="#">Your Name Here</a>.
-    </div> */}
-    </>
+    <div className="container">
+      <div className="card">
+        <div className="card_image">
+          <div className="card_image_mobile"></div>
+          <div className="card_image_desktop">
+            <div className="card_image_box_svg"></div>
+          </div>
+        </div>
+        <article className="card_info">
+          <h1 className="card_info_title">FAQ</h1>
+
+          {accordionData.map((item, index) => (
+            <section
+              key={index}
+              className={`card_info_item ${activeItem === index ? 'active' : ''}`}
+              onClick={() => toggleAccordion(index)}
+            >
+              <h2 className={`card_info_item_title ${activeItem === index ? 'bolder' : ''}`}>
+                {item.question}
+              </h2>
+              <img
+                src={iconArrowDown}
+                alt="icon"
+                className={`card_info_item_icon ${activeItem === index ? 'rotate' : ''}`}
+              />
+              <p className={`card_info_item_para ${activeItem === index ? 'show_info' : ''}`}>
+                {item.answer}
+              </p>
+              <hr className="card_info_divider" />
+            </section>
+          ))}
+        </article>
+      </div>
+    </div>
   );
 }
 export default App;
